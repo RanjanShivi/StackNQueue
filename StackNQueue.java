20400d68fec9fe30f88d0e530e24dca5d329c3e0
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-31 00:27:05 +0530

    push operstion is performed

---
+++ StackNQueue.java
@@ -0,0 +1,13 @@
+package com;
+
+public class StackNQueue {
+    public static void main(String[] args) {
+        StackUsingLinkList list = new StackUsingLinkList();
+
+        System.out.println("PUSH");
+        list.push(70);
+        list.push(30);
+        list.push(56);
+        list.display();
+    }
+}
